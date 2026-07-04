addea45ec1c2d4add89e4c8028b4f8f76f1085c7
saputt <sauki666indo@gmail.com>
2026-07-04 17:12:15 +0700

    feat: update main.py with new API routers (analysis, embedding, job) and FastAPI metadata

--- AIService/app/main.py
+++ AIService/app/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.features.market.router import router as market_router
+from app.api.job_routes import router as ai_router
+from app.api.embedding_routes import router as embedding_router
+from app.api.analysis_routes import router as analysis_router
+
 import uvicorn
 from app.core.config import Settings
 
-app = FastAPI()
+app = FastAPI(
+    title="Hyperlocal IT Matchmaker AI API",
+    description="Backend AI Pipeline & Services Platform menggunakan Clean Architecture SoC",
+    version="1.0.0"
+)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://localhost:8080"],
@@ -13,12 +20,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(market_router)
+app.include_router(ai_router)
+app.include_router(embedding_router)
+app.include_router(analysis_router)
 
 if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=Settings.PORT | 5000,
+        port=Settings.PORT or 5000,
         reload=True
     )
